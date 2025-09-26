91556634e40c79ff8605712d178692cd53d623a8
rinshadkl <amjedvnml@gmail.com>
2025-09-26 21:36:12 +0530

    Fix: Use correct MongoDB URI env var

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
